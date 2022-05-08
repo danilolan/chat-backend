92f17f787758c22b106f1d62cdfd7baa5aa7a66d
danilo.herculano2 <danilo.herculano@uaubox.com.br>
2022-05-08 19:27:05 -0300

    add server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('isddsa')
+import express from 'express'
+import http from 'http'
+import { Server as ServerIo } from 'socket.io'
+
+const port = 3001
+
+const app = express()
+const server = http.createServer(app)
+const io = new ServerIo(server)
+
+server.listen(port, () => console.log('Server is listen on port: ', port))
