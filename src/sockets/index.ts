9f12cafb6db49eb6482e14ed5b0a7d065044fbcb
danilo.herculano2 <danilo.herculano@uaubox.com.br>
2022-05-09 22:47:42 -0300

    add chat system

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,13 +1,24 @@
-// import chatController from './chat'
+import { Socket } from 'socket.io'
+
+const msgData = []
 
 export default function sockets (io) {
   io.on('connection', (socket) => {
     console.log('Client connect ', socket.id)
 
-    
+    chatMessages(socket, io)
 
     socket.on('disconnect', (socket) => {
-      console.log('Client disconnect', socket.id)
+      console.log('Client disconnect')
     })
+  })
+}
+
+function chatMessages (socket : Socket, io) {
+  io.emit('receivedMessage', msgData)
+
+  socket.on('sendMessage', (msg) => {
+    msgData.push(msg)
+    io.emit('receivedMessage', msgData)
   })
 }
