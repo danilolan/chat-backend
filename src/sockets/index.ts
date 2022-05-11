f43fe2d05fc61b2d6bbba4d9878a4c2ba90f607e
danilo.herculano2 <danilo.herculano@uaubox.com.br>
2022-05-11 00:09:42 -0300

    feature rooms

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,24 +1,38 @@
 import { Socket } from 'socket.io'
 
-const msgData = []
+const users = []
+
+const messages = {
+  room1: [],
+  room2: []
+}
 
 export default function sockets (io) {
   io.on('connection', (socket) => {
     console.log('Client connect ', socket.id)
 
-    chatMessages(socket, io)
+    socket.on('join room', (roomName, callback) => {
+      socket.join(roomName)
+      console.log(`The socket ${socket.id} enter in chat ${roomName}`)
+      callback(messages[roomName])
+    })
+
+    socket.on('send message', ({ roomName, author, content }) => {
+      const payload = {
+        content,
+        roomName,
+        author
+      }
+
+      if (messages[roomName]) {
+        messages[roomName].push(payload)
+      }
+
+      io.sockets.in(roomName).emit('new message', messages[roomName])
+    })
 
     socket.on('disconnect', (socket) => {
       console.log('Client disconnect')
     })
-  })
-}
-
-function chatMessages (socket : Socket, io) {
-  io.emit('receivedMessage', msgData)
-
-  socket.on('sendMessage', (msg) => {
-    msgData.push(msg)
-    io.emit('receivedMessage', msgData)
   })
 }
